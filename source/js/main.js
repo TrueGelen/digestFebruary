3c54cb15f3fa0ff92e12f27298e0cb1145b40fa2
TrueGelen <54402826+TrueGelen@users.noreply.github.com>
2020-02-28 16:33:25 +0300

    some

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
+import './pagination'
+import './makeButtonsLink'
 import './positionBurger'
 import './menu'
-import './slide4PlayVideo'
+// import './slide4PlayVideo'
 import './removePlugin'
 // import './scrollTo'
