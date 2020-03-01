7a2b6430993dc11c82820d8e15cace61d692e68c
Vlad Filchagin <gelenbagen@gmail.com>
2020-03-01 16:46:47 +0300

    last v

--- source/js/makeButtonsLink.js
+++ source/js/makeButtonsLink.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
   const sections = document.querySelectorAll('section')
 
-  for(let i = 1; i < sections.length; i++){
+  for (let i = 1; i < sections.length - 1; i++) {
     let buttons = sections[i].querySelectorAll('.slide__buttons a')
     buttons[0].setAttribute('href', `#${sections[i - 1].id}`)
     buttons[1].setAttribute('href', `#${sections[i + 1].id}`)
--- source/js/menu.js
+++ source/js/menu.js
@@ -2,9 +2,10 @@ window.addEventListener('load', () => {
 
   const burger = document.querySelector('#burger')
   const menu = document.querySelector('#menu')
+  const menuItems = menu.querySelectorAll('.menu__items a')
   const close = menu.querySelector('.menu__close')
 
-  burger.addEventListener('click', () => {
+  function showMenu() {
     burger.style.opacity = 0
 
     menu.classList.remove('hideMenu')
@@ -12,10 +13,9 @@ window.addEventListener('load', () => {
 
     close.classList.remove('hideClose')
     close.classList.add('showClose')
-    
-  })
+  }
 
-  close.addEventListener('click', () => {
+  function hideMenu() {
     burger.style.opacity = 1
 
     menu.classList.remove('showMenu')
@@ -23,6 +23,16 @@ window.addEventListener('load', () => {
 
     close.classList.remove('showClose')
     close.classList.add('hideClose')
-  })
+  }
+
+  burger.addEventListener('click', showMenu)
+
+  close.addEventListener('click', hideMenu)
+
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+      hideMenu()
+    })
+  }
 
 })
